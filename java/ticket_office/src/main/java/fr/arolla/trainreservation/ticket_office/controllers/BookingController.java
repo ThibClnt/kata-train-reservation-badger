278bdfb81b5fa79363134bdfcdf6820776a7e8d5
talla <talla.diop@efrei.net>
2024-11-20 11:03:07 +0100

    Refactor domain logic with service & controller

--- java/ticket_office/src/main/java/fr/arolla/trainreservation/ticket_office/controllers/BookingController.java
+++ java/ticket_office/src/main/java/fr/arolla/trainreservation/ticket_office/controllers/BookingController.java
@@ -1,20 +1,9 @@
 package fr.arolla.trainreservation.ticket_office.controllers;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import fr.arolla.trainreservation.ticket_office.BookingService;
-import fr.arolla.trainreservation.ticket_office.HttpClient;
-import fr.arolla.trainreservation.ticket_office.RestClient;
-import fr.arolla.trainreservation.ticket_office.Seat;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 @RestController
